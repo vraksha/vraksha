fa174d01619310d4aa1bb274c0877acc22361007
Cybro <cybro.dev7@gmail.com>
2026-04-02 23:06:36 +0545

    Feat: Now agent can read and update file content as per request

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import re
 from pathlib import Path
 
 import sys
@@ -7,34 +8,58 @@ import io
 from anthropic import Anthropic
 
 from extract_api import get_api_key
-from forensic_prompt import forensic_prompt
+from prompts import Prompts
 from extract_content import content_extractor
+from changes import apply_changes
 
 API_KEY = get_api_key(".env.local")
 
 client = Anthropic(api_key=API_KEY)
 
-FORENSIC_PROMPT = forensic_prompt()
+# FORENSIC_PROMPT = Prompt.forensic()
 
 # print(content_extractor(filename="rules"))
 
-def call_llm(extracted_content):
+def call_llm(project, memory, rules):
     response = client.messages.create(
-        model="claude-opus-4-6",
+        model="claude-sonnet-4-5",
         max_tokens=1500,
-        system=FORENSIC_PROMPT,
+        system=f"""
+            {Prompts.system()}
+
+            <file_list>
+                <file name="projects.yaml">
+                    {project}
+                </file>
+
+                <file name="memory.yaml">
+                    {memory}
+                </file>
+
+                <file name="rules.md">
+                    {rules}
+                </file>
+
+            </file_list>
+        """
+        ,
         messages=[
             {
                 "role": "user",
-                "content": f"Analyze this file and answer in short, what's the current state of my latest project? {extracted_content}"
+                "content": "Analyze the 3 given files and since now I know that what ysws submissions give to reviewers(which you don't need to know, so just proceed with updating if needed), update the relevant file with the text 'Will my agent perform good?' into relevant area of that file And update the date with today's date"
             }
             ],
     )
 
-    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+    response_text = response.content[0].text
+    apply_changes(response_text)
 
-    print(response.content[0].text)
+    return(response_text)
 
-extracted_content = content_extractor(filename="projects")
+project = content_extractor(filename="projects")
+memory = content_extractor(filename="memory")
+rules = content_extractor(filename="rules")
 
-call_llm(extracted_content)
+
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+print(call_llm(project, memory, rules))
