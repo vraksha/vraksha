88b9e8da037900dd75cbcb2ab8507ef1fd7bf662
Cybro <cybro.dev7@gmail.com>
2026-04-03 16:09:18 +0545

    Moved imports and api key loading outside of function and changed files position in system prompt

--- src/llm.py
+++ src/llm.py
@@ -1,22 +1,17 @@
+import sys, io
+from anthropic import Anthropic
+from src.prompts import Prompts
+from src.utils.extract_api import get_api_key
+from src.utils.extract_content import content_extractor
+from src.utils.changes import apply_changes
 
-def call_llm(user_prompt):
-    
-    import sys, io
+# Getting the api key
+API_KEY = get_api_key(".env.local")
 
-    from anthropic import Anthropic
+# Initializing the client
+client = Anthropic(api_key=API_KEY)
 
-    from src.prompts import Prompts
-
-    from src.utils.extract_api import get_api_key
-
-    from src.utils.extract_content import content_extractor
-    from src.utils.changes import apply_changes
-
-    # Getting the api key
-    API_KEY = get_api_key(".env.local")
-
-    # Initializing the client
-    client = Anthropic(api_key=API_KEY)
+def call_llm(messages) -> str:
 
     # Getting the project, memory and rules
     project = content_extractor(filename="projects")
@@ -31,6 +26,10 @@ def call_llm(user_prompt):
             {Prompts.system()}
 
             <file_list>
+                <file name="rules.md">
+                    {rules}
+                </file>
+
                 <file name="projects.yaml">
                     {project}
                 </file>
@@ -39,25 +38,16 @@ def call_llm(user_prompt):
                     {memory}
                 </file>
 
-                <file name="rules.md">
-                    {rules}
-                </file>
 
             </file_list>
         """
         ,
-        messages=[
-            {
-                "role": "user",
-                "content": user_prompt
-            }
-            ],
+        messages=messages,
     )
 
     response_text = response.content[0].text
-    apply_changes(response_text)
 
-    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+    apply_changes(response_text)
 
     return response_text
 
