0cc89eb50136c85fe4115848a9d1f378996a334c
Cybro <cybro.dev7@gmail.com>
2026-04-03 18:46:46 +0545

    Made it a root to run the agent

--- main.py
+++ main.py
@@ -1,33 +1,16 @@
+# Agent is present in:
+# "src/agent/loop.py"
+
+
+# Slop detector to detect ai slop code vs human is present in:
+# "src/slop_detector/detector.py"
+
 import io
 import sys
 
-from src.llm import call_llm
-from src.utils.changes import apply_changes
+from src.agent.loop import run
 
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
-messages = []
-
-while True:
-    print("\n======= Enter 'quit' or 'exit' or 'bye' to exit =======\n")
-
-    prompt = input("Ask something:\n")
-
-    if prompt.lower() in ["quit", "exit", "bye"]:
-        if len(messages) != 0:
-            messages.append({
-                "role": "user", "content": "Session ending. Rewrite memory.yaml with a clean compressed summary of everything discussed this session. Update projects.yaml if anything changed."
-                })
-
-            response_text = call_llm(messages)
-
-        break
-
-    messages.append({"role": "user", "content": prompt})
-    
-    response_text = call_llm(messages)
-
-    messages.append({"role": "assistant", "content": response_text})
-
-    print(f"\nAgent:\n{response_text}")
-
+if __name__ == "__main__":
+    run()
