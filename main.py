5dbd4123e247e5485b5626a342ef27d9087786b6
Cybro <cybro.dev7@gmail.com>
2026-04-07 17:53:06 +0545

    Feat: Transition to multiagent cognitive architecture with automated memory recalibration

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
-# Agent is present in:
-# "src/agent/loop.py"
-
-
-# Slop detector to detect ai slop code vs human is present in:
-# "src/slop_detector/loop.py"
-
 import io
 import sys
 
-from src.agent.loop import run_agent
+from src.agent.loop import run_agent # For general Agent
 
-from src.slop_detector.loop import run_detector
+# from src.slop_detector.loop import run_detector # For AI slop detector 
 
-# from src.utils.prepare_for_llm import PrepareForLLM
+from src.utils.prepare_for_llm import PrepareForLLM
 
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
-if __name__ == "__main__":
 
+# Now the general agent will call the slop detector by itself if it has to analyze github repo,
+## User doesn't have to call slop detector specifically
+
+
+if __name__ == "__main__":
     run_agent()
