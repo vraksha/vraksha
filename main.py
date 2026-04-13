5ca6539480dd817b8b2f749eae531e0d72d4be2a
Cybro <cybro.dev7@gmail.com>
2026-04-13 17:27:03 +0545

    Fix the code to make it modular, now adding a new skill with the same structure and modules as slop_detector is enough

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import io
 import sys
 
-from src.agent.loop import run_agent # For general Agent
-# from src.skills.slop_detector.loop import run_detector # For AI slop detector 
-# from src.utils.base_loop import run_loop # For orchestrator
-from src.skills.slop_detector.services.prepare_for_llm import PrepareForLLM
+import logging
+
+from src.agent.loop import run_agent # For Agent (orchestrator)
 
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
-
-# Now the general agent will call the slop detector by itself if it has to analyze github repo,
-## User doesn't have to call slop detector specifically
-
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s"
+)
 
 if __name__ == "__main__":
     run_agent()
