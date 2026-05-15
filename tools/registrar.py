d0d9af633ad8ea00848e08006a8f0c42db8110c2
thecybro <cybro.dev7@gmail.com>
2026-05-15 16:24:54 +0545

    Create a unique module name generator to generate tool names based on path to avoid collisions

--- tools/registrar.py
+++ tools/registrar.py
@@ -4,21 +4,29 @@ logger = logging.getLogger(__name__)
 
 import importlib.util as util
 
+from tool_name import get_tool_name
+
 from get_root import root
 
 TOOLS_DIR = (root.project) / "tools"
 
 FILES_TO_IGNORE = ["base.py", "registrar.py", "registry.py", "__init__.py", "command_tool.py", "create_sandbox.py"]
 
+FOLDERS_TO_IGNORE = ["resolve"]
+
 def discover_tools():
     for tool_file in TOOLS_DIR.rglob("*.py"):
-        if tool_file.name not in FILES_TO_IGNORE:
+        if (
+            tool_file.name not in FILES_TO_IGNORE
+            and tool_file.parent.name not in FOLDERS_TO_IGNORE
+        ):
             yield tool_file
 
 def register_tools():
     for tool_file in discover_tools():
-        tool_name = tool_file.stem
-        
+        # tool_name = tool_file.stem
+        tool_name = get_tool_name(tool_file, "tools", "farthest")
+
         spec = util.spec_from_file_location(tool_name, tool_file)
 
         if spec is None or spec.loader is None:
@@ -26,14 +34,6 @@ def register_tools():
 
         module = util.module_from_spec(spec)
         spec.loader.exec_module(module)
-
-        """
-        # Create a unique module name based on path to avoid collisions
-        # e.g., "src.tools.file_tools.read"
-        module_path = f"dynamic_tool_{tool_file.stem}_{hash(str(tool_file))}"
-        
-        spec = util.spec_from_file_location(module_path, tool_file)
-        """
 
         yield {
             "name": tool_name,
